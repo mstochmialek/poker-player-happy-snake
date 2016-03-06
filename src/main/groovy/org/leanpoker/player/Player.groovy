a563d103cc7f6c1f941ec91368e946a683b6284e
mst <mst@avaleo.net>
2016-03-06 11:11:13 +0100

    first strategy based on high cards

--- src/main/groovy/org/leanpoker/player/Player.groovy
+++ src/main/groovy/org/leanpoker/player/Player.groovy
@@ -2,10 +2,23 @@ package org.leanpoker.player
 
 class Player {
 
-    static final String VERSION = 'Default Groovy folding player';
+    static final String VERSION = 'Default Groovy folding player 1.1';
+
 
     static int betRequest(def gameState) {
-        1000
+
+        String myHand = hand(gameState);
+        if (myHand.contains('A') || myHand.contains('K')) {
+            return gameState.minimum_raise
+        }
+
+        0
+    }
+
+    static String hand(def gameState) {
+        List myCarts = gameState.players[gameState.in_action].hole_cards;
+
+        myCarts[0].rank + myCarts[1].rank;
     }
 
     static void showdown(def gameState) {
