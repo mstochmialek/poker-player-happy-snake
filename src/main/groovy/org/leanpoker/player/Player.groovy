aeaba1d715f80a10513d61e0eb71a6d8d760e2fb
Tomasz Klewin <tkl@avaleo.net>
2016-03-06 14:06:32 +0100

    +5

--- src/main/groovy/org/leanpoker/player/Player.groovy
+++ src/main/groovy/org/leanpoker/player/Player.groovy
@@ -4,7 +4,6 @@ class Player {
 
     static final String VERSION = 'Leanforge 1.8';
 
-
     static int betRequest(def gameState) {
         try {
             def myHand = hand2(gameState);
@@ -18,7 +17,7 @@ class Player {
 
     static int doBetRequest(List<Card> myHand, List<Card> table, int minimum) {
         if (myHand[0] == myHand[1]) {
-            return Integer.MAX_VALUE
+            return minimum + 5;
         }
 
         if (myHand.collect {it.rank}.any { it == Rank.ACE || it == Rank.KING || it == Rank.QUEEN || it == Rank.JACK}) {
