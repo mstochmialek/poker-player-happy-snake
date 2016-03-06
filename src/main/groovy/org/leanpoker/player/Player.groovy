4e6821060760fe6e9f33551d9b9f741951efa028
Tomasz <tkl@avaleo.net>
2016-03-06 10:44:11 +0100

    Update Player.groovy

--- src/main/groovy/org/leanpoker/player/Player.groovy
+++ src/main/groovy/org/leanpoker/player/Player.groovy
@@ -5,7 +5,7 @@ class Player {
     static final String VERSION = 'Default Groovy folding player';
 
     static int betRequest(def gameState) {
-        0
+        100
     }
 
     static void showdown(def gameState) {
