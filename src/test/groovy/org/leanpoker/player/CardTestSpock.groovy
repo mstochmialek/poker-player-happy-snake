f1d851ccd21b46389d9ba0c4efbb0549620b6052
mst <mst@avaleo.net>
2016-03-06 13:56:44 +0100

    higher method on Card, Rank, Suit

--- src/test/groovy/org/leanpoker/player/CardTestSpock.groovy
+++ src/test/groovy/org/leanpoker/player/CardTestSpock.groovy
@@ -3,6 +3,9 @@ package org.leanpoker.player
 import groovy.json.JsonSlurper
 import spock.lang.Specification
 
+import static org.leanpoker.player.Rank.*
+import static org.leanpoker.player.Suit.*
+
 /**
  * Java/Groovy unit test using the Spock framework.
  * https://code.google.com/p/spock/
@@ -19,8 +22,8 @@ class CardTestSpock extends Specification {
 		def card = new Card(cardJson);
 
 		expect:
-		card.suit == Suit.SPADES
-		card.rank == Rank.C04
+		card.suit == SPADES
+		card.rank == C04
 	}
 
 	def "Equals"() {
@@ -32,4 +35,22 @@ class CardTestSpock extends Specification {
 		expect:
 		card1 == card2
 	}
+
+	def "Equals based on enums"() {
+		given:
+
+		def card1 = new Card(C04, SPADES)
+		def card2 = new Card(C04, HEARTS)
+
+		expect:
+		card1 == card2
+	}
+
+	def "higher"() {
+		expect:
+		new Card(ACE, HEARTS).higher(new Card(KING, HEARTS))
+		new Card(ACE, HEARTS).higher(new Card(ACE, SPADES))
+		!new Card(KING, HEARTS).higher(new Card(ACE, HEARTS))
+	}
+
 }
